bade894d2968f48cec604a461de4cb870c56468c
Eddie <Eddie@Edwins-MacBook-Pro.local>
2017-09-29 20:03:05 -0700

    Added documentation to autocomplete tree

--- misc_functions/triadAutoComplete.js
+++ misc_functions/triadAutoComplete.js
@@ -1,10 +1,15 @@
 'use strict';
 
+/*
+This is the functionality for auto complete. It creates a tree where each node is the character of a word.
+A library of values must be passed into the tree to populate the nodes.
+*/
 const TriadTree = function() {
   this.alphaHash = {};
 };
 
 TriadTree.prototype.loadLibrary = function(library) {
+  //Takes an array of values to populate the tree
   if (!Array.isArray(library)) return console.log('Input Must be An Array');
   library.forEach(entry => {
     this.loadWord(entry);
@@ -12,10 +17,11 @@ TriadTree.prototype.loadLibrary = function(library) {
 };
 
 TriadTree.prototype.loadWord = function(word) {
+  //Takes a word and forms a chain of nodes where the last node in the chain has a
+  //wordEnd value of true.
   if (!word) return console.log('No word passed');
   if (word.length === 0) return console.log('passed an empty string');
   if (!this.alphaHash[word[0]]) this.alphaHash[word[0]] = new alphaNode(word[0]);
-
 
   const _loadWordCharacters = (node, word, ind) => {
     let char = word[ind];
@@ -31,11 +37,8 @@ TriadTree.prototype.loadWord = function(word) {
     return _loadWordCharacters(node.children[char], word, ind + 1);
   };
 
-
   return _loadWordCharacters(this.alphaHash[word[0]], word, 1);
 };
-
-
 
 TriadTree.prototype.searchWords = function(word) {
   if (!word) return console.log('No Word Passed!!');
@@ -50,6 +53,9 @@ TriadTree.prototype.searchWords = function(word) {
 
 
 TriadTree.prototype._recursiveWordSearch = function(node, word, buildingWord, ind, output) {
+  /*Recursive function that traverses a tree and adds every value to an empty String
+  If the current node has a wordEnd value of true, the current version of the buildingWord
+  is pushed to the output array which is returned in the end.*/
   buildingWord += node.value;
   let nextChar = word[ind + 1];
 
@@ -58,7 +64,7 @@ TriadTree.prototype._recursiveWordSearch = function(node, word, buildingWord, in
     return this._recursiveWordSearch(newNode, word, buildingWord, ind + 1, output);
   }
 
-  if (node.wordEnd) output.push(buildingWord);
+  node.wordEnd ? output.push(buildingWord) : null;
   if (Object.keys(node.children).length === 0) return output;
 
   for (let child in node.children) {
@@ -66,7 +72,6 @@ TriadTree.prototype._recursiveWordSearch = function(node, word, buildingWord, in
   }
   return output;
 };
-
 
 const alphaNode = function(char, wordEnd=false) {
   this.value = char;
