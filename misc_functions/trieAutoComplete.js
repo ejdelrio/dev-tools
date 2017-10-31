0c63ca5f5e48a615a00309b3eb1fbc1605de38d2
ejdelrio <edwinjdelrio@gmail.com>
2017-10-31 16:36:52 -0700

    Update trieAutoComplete.js

--- misc_functions/trieAutoComplete.js
+++ misc_functions/trieAutoComplete.js
@@ -8,38 +8,32 @@ const TrieTree = function() {
   this.alphaHash = {};
 };
 
-TrieTree.prototype.loadLibrary = function(library) {
-  // Takes an array of values to populate the tree
-  if (!Array.isArray(library)) return console.log('Input Must be An Array');
-  library.forEach(entry => {
-    this.loadWord(entry);
-  });
-};
-
-TrieTree.prototype.loadWord = function(word) {
+TrieTree.prototype.loadWord = function(obj, stringVal) {
   // Takes a word and forms a chain of nodes where the last node in the chain has a
   // wordEnd value of true.
-  if (!word) return console.error('No word passed');
-  if (typeof word !== 'string') return console.error('Input must be a string');
-  if (!this.alphaHash[word[0]]) this.alphaHash[word[0]] = new alphaNode(word[0]);
+  if (!obj) return console.error('No object passed');
+  if(!stringVal) return console.error('No string value passed');
+  if(typeof stringVal != 'string') return console.error('Second input must be a string');
+  
+  if (!this.alphaHash[stringVal[0]]) this.alphaHash[stringVal[0]] = new alphaNode(stringVal[0]);
 
-  const _loadWordCharacters = (node, word, ind) => {
-    let char = word[ind];
+  const _loadWordCharacters = (node, ind) => {
+    let char = stringVal[ind];
 
     if (node.children[char]) {
-      return !word[ind + 1] ?
-      node.children[char].wordEnd = true:
-      _loadWordCharacters(node.children[char], word, ind + 1);
+      return !stringVal[ind + 1] ?
+      node.children[char].wordEnd = obj:
+      _loadWordCharacters(node.children[char], ind + 1);
     }
 
     node.children[char] = new alphaNode(char);
 
-    return !word[ind + 1] ?
-    node.children[char].wordEnd = true:
-    _loadWordCharacters(node.children[char], word, ind + 1);
+    return !stringVal[ind + 1] ?
+    node.children[char].wordEnd = obj:
+    _loadWordCharacters(node.children[char], ind + 1);
   };
 
-  return _loadWordCharacters(this.alphaHash[word[0]], word, 1);
+  return _loadWordCharacters(this.alphaHash[stringVal[0]], 1);
 };
 
 
@@ -49,32 +43,21 @@ TrieTree.prototype.searchWords = function(word) {
   if (!this.alphaHash[word[0]]) return [];
   let output = [];
 
-  function _wordSearch(node, autoWord, ind) {
-    autoWord += node.value;
+  function _wordSearch(node, ind) {
     let nextChar = word[ind + 1];
     let {children} = node;
     let numberOfChildren = Object.keys(children).length;
 
     if (nextChar && !children[nextChar]) return output;
-    if (children[nextChar]) return _wordSearch(children[nextChar], autoWord, ind + 1);
-    if (node.wordEnd) output.push(autoWord);
+    if (children[nextChar]) return _wordSearch(children[nextChar], ind + 1);
+    if (node.wordEnd) output.push(node.wordEnd);
     if (numberOfChildren === 0) return output;
 
     for (let child in children) {
-      _wordSearch(children[child], autoWord, ind + 1);
+      _wordSearch(children[child], ind + 1);
     }
     return output;
   }
 
-  return _wordSearch(this.alphaHash[word[0]], '', 0);
+  return _wordSearch(this.alphaHash[word[0]], 0);
 };
-
-
-const alphaNode = function(char, wordEnd=false) {
-  this.value = char;
-  this.wordEnd = wordEnd;
-  this.children = {};
-};
-
-
-module.exports = TrieTree;
